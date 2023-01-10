8aa8d23bb3401ff846b11d0f77f272d0cf80b5ba
0xbingo <ryuheimat3@yahoo.com>
2023-01-10 11:37:05 +0900

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,26 @@
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-etherscan';
-import '@typechain/hardhat';
-import 'hardhat-deploy';
-import 'solidity-coverage';
-import 'dotenv/config';
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "solidity-coverage";
+import "dotenv/config";
 
 export default {
   networks: {
     hardhat: {
       gas: 10000000,
       accounts: {
-        accountsBalance: '1000000000000000000000000',
+        accountsBalance: "1000000000000000000000000",
       },
       allowUnlimitedContractSize: true,
       timeout: 6000000,
     },
   },
   typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
+    outDir: "typechain",
+    target: "ethers-v5",
   },
   solidity: {
-    version: '0.8.16',
+    version: "0.8.16",
     settings: {
       optimizer: {
         enabled: true,
